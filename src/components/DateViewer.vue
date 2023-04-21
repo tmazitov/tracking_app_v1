32b4ee46768aac959a4df9469df5f147f8414c2c
Timur Mazitov <timurmazitov000@gmail.com>
2023-04-21 18:05:47 +0600

    update DateViever : update styles and added the type of date

--- src/components/DateViewer.vue
+++ src/components/DateViewer.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="date__viewer">
 		{{ dateString }}
-		<a style="color:grey">{{timeString}}</a>
+		<a style="color:grey">{{ timeString }}</a>
 	</div>
 </template>
 
@@ -9,51 +9,83 @@
 import { ref } from 'vue';
 
 const namesOfWeekDays = [
-	'Вс.',
-	'Пн.',
-	'Вт.',
-	'Ср.',
-	'Чт.',
-	'Пт.',
-	'Сб.',
+	'вс.',
+	'пн.',
+	'вт.',
+	'ср.',
+	'чт.',
+	'пт.',
+	'сб.',
 ]
+
+function checkDate(date:Date) {
+	const today = new Date();
+	const tomorrow = new Date(today);
+	tomorrow.setDate(tomorrow.getDate() + 1);
+	const yesterday = new Date(today);
+	yesterday.setDate(yesterday.getDate() - 1);
+
+	if (date.toDateString() === today.toDateString()) {
+		return "Сегодня";
+	} else if (date.toDateString() === tomorrow.toDateString()) {
+		return "Завтра";
+	} else if (date.toDateString() === yesterday.toDateString()) {
+		return "Вчера";
+	} else {
+		return -1;
+	}
+}
 
 const namesOfMonths = [
-	'Января',
-	'Февраля',
-	'Марта',
-	'Апреля',
-	'Мая',
-	'Июня',
-	'Июля',
-	'Августа',
-	'Сентября',
-	'Октября',
-	'Ноября',
-	'Декабря',
+	'января',
+	'февраля',
+	'марта',
+	'апреля',
+	'мая',
+	'июня',
+	'июля',
+	'августа',
+	'сентября',
+	'октября',
+	'ноября',
+	'декабря',
 ]
 
-const getDateString:Function = (date:Date) => {
-	
-	let month:String = namesOfMonths[date.getMonth()]
-	let dayOfWeek:String = namesOfWeekDays[date.getDay()]
+const getDateString: Function = (date: Date) => {
+
+	let month: String = namesOfMonths[date.getMonth()]
+	let dayOfWeek: String = namesOfWeekDays[date.getDay()]
 
 	return [dayOfWeek, date.getDate(), month, date.getFullYear()].join(" ")
 }
 
 export default {
 	name: 'TrackingAppV1DateViewer',
-	setup(){		
+	props: {
+		date: {
+			type: Date,
+			required: true,
+		}
+	},
+	setup(props) {
 		const dateString = ref("")
 		const timeString = ref("")
 		setInterval(() => {
-			let date:Date = new Date()
-			dateString.value = getDateString(date)
-			if (date.getMinutes() > 9){
+			let date: Date = props.date
+
+			let dateType = checkDate(date)
+			if (dateType != -1){
+				dateString.value = dateType + " " + getDateString(date)
+			}
+			else {
+				dateString.value = getDateString(date)
+			}
+			if (date.getMinutes() > 9) {
 				timeString.value = `${date.getHours()}:${date.getMinutes()}`
 			} else {
 				timeString.value = `${date.getHours()}:0${date.getMinutes()}`
 			}
+
 		}, 1000)
 
 
@@ -66,10 +98,10 @@ export default {
 </script>
 
 <style scoped>
-.date__viewer{
-	font-size: 24px;
+.date__viewer {
+	font-size: 18px;
 	font-weight: 500;
-	margin: 20px;
-	width: 280px;
+	margin: 10px;
+	width: calc(100% - 20px);
 }
 </style>
